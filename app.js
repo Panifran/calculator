7e68793b7ee92d85cdda73cf1dfa608f81bce7a3
Javier <panifran2000@gmail.com>
2022-05-04 19:22:24 +0200

    Add equals and clear button

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const zero = document.querySelector('#zero');
 const one = document.querySelector('#one');
 const two = document.querySelector('#two');
 const three = document.querySelector('#three');
@@ -12,20 +13,30 @@ const add = document.querySelector('#add');
 const subtract = document.querySelector('#subtract');
 const divide = document.querySelector('#divide');
 const multiply = document.querySelector('#multiply');
+const equals = document.querySelector('#equals');
+const screenBottom = document.querySelector('#screen-bottom');
+const screenTop = document.querySelector('#screen-top');
+const clear = document.querySelector('#clear');
 
-let number = 0.0;
-let updateNumber = 0.0;
+let number = 0;
+let updateNumber = 0;
 let operation = '+';
+let decimalNumber;
 
-const numbers = [one, two, three, four, five, six, seven, eight, nine, decimal];
+const numbers = [zero, one, two, three, four, five, six, seven, eight, nine, decimal];
 
 for (let num of numbers) {
   num.addEventListener('click', function () {
     number += num.textContent;
     decimalNumber = parseFloat(number);
     console.log(parseFloat(number));
+    screenBottom.textContent = '';
+    screenTop.textContent = decimalNumber;
+    
   });
+  
 }
+
 function operate() {
   if (operation == '+') {
     updateNumber += decimalNumber;
@@ -42,7 +53,9 @@ add.addEventListener('click', function () {
   operate();
   console.log(updateNumber);
   operation = '+';
+  screenTop.textContent += ' + ';
   number = 0;
+
 });
 
 subtract.addEventListener('click', function () {
@@ -65,3 +78,18 @@ divide.addEventListener('click', function () {
   operation = '/';
   number = 0;
 });
+
+equals.addEventListener('click', function () {
+  operate();
+  console.log(updateNumber);
+  number = 0;
+  screenBottom.textContent = updateNumber;
+  updateNumber = 0;
+});
+
+clear.addEventListener('click', function() {
+  number = 0;
+  screenBottom.textContent = '';
+  screenTop.textContent = '';
+  updateNumber = 0;
+})
